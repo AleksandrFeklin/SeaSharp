636b9bde2526fb50449abfda7644df83a6fd8bce
Aleksandr <i@afeklin.ru>
2022-08-11 19:09:58 +0300

    DZ2

---
+++ Lesson2Dz/10/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+Random random = new Random();
+
+int randomNumber = random.Next(100, 1000); 
+Console.WriteLine ( "Случайное трёхзначное число = "+randomNumber);
+
+int result = (randomNumber % 100) /10;
+
+Console.WriteLine(result);
+
+// Console.WriteLine("Введите трёхзначное число");
+
+// string? numberString = Console.ReadLine();
+
+// int number = int.Parse(numberString!);
+
+// int firstNumber = (number / 100) * 100; 
+
+// int result = (number -  firstNumber) / 10; 
+
+// Console.WriteLine("Результат = " + result); 
---
+++ Lesson2Dz/13/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+Console.WriteLine ("Введите число: ");
+string? numberString = Console.ReadLine ();
+int number=  int.Parse (numberString!);
+
+if (number>999)
+{
+     while (number > 999)
+     {
+        number = (number/10);
+     }
+}
+
+if (number<0)
+    number = number*-1;
+if (number<100)
+    Console.WriteLine ("Третья цифра отсутствует ");
+else
+    Console.WriteLine (number%10);
