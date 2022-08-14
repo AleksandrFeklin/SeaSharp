fb10f42d0d9d8276cc5f61a9e7ce798b808b5d4b
Aleksandr <i@afeklin.ru>
2022-08-14 20:25:03 +0300

    Add DZ3

---
+++ Lesson3Dz/19/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 19
+
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+Console.WriteLine("Введите пятизначное число:");
+var number = int.Parse(Console.ReadLine()!);
+
+int number1 = number / 10000;                            
+int number2 = number / 1000 - number1 * 10; 
+int number3 = (number/100) - ((number/1000)*10);             
+int number4 = (number - (number / 100 * 100)) / 10;     
+int number5 = number % 10;                               
+
+if (number > 99999 || number < 10000)
+{
+    Console.WriteLine("Ошибка, не правильное число");
+    Console.WriteLine();
+    Console.WriteLine("Введите пятизначное число");
+}
+
+else if (number1 == number5 && number2 == number4)
+{
+    Console.WriteLine("Является Полиндромом");
+}
+else
+{
+    Console.WriteLine("Не является Полиндромом");
+}
+
+
+
---
+++ Lesson3Dz/23/Program.cs
@@ -0,0 +1,30 @@
+﻿// // Задача 23
+
+// // Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// // 3 -> 1, 8, 27
+// // 5 -> 1, 8, 27, 64, 125
+
+
+Console.WriteLine("Введите число ");
+string? numberString = Console.ReadLine()!;
+int x = int.Parse(numberString);
+int index = 1;
+
+if (x < 1)
+{
+    Console.WriteLine (" Ошибка: Число должно быть больше  0 " );
+   return;
+}
+while (x >= index)
+{
+
+    var result = Math.Pow(index, 3);
+    index++;
+    Console.WriteLine(result);
+}
+
+
+
+
+
